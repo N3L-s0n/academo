90e0f41e589863af50bda6edf9a98460d59c609c
Nelson Alvarez <nelson.alvarezcedeno@gmail.com>
2023-01-04 19:53:09 -0500

    Navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+        <Navbar />
         <p>
             Bienvenidos
         </p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <div className="navbar">
+                <h3>Academo.</h3>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Contactanos</li>
+                    <li>Ayuda</li>
+                </ul>
+                <p>Ingresar</p>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
