2ff4233992226d61bc38d7c158a632b96fb10c25
Nelson Alvarez <nelson.alvarezcedeno@gmail.com>
2023-01-06 15:50:59 -0500

    Added about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar'
-import Welcome from './components/Welcome';
+import Navbar from './components/Navbar/Navbar'
+import Welcome from './components/Welcome/Welcome'
+import About from './components/About/About'
 
 const App = () => {
   return (
     <div className="App">
         <Navbar />
         <Welcome />
+        <About />
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import './About.css'
+
+const About = () => {
+    return(
+        <div className='about'>
+            <h1>¿Quiénes somos?</h1>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+            <span>Todo lo que se llama estudiar y aprender no es otra cosa que recordar. (Platón 427 AC - 347 AC)</span>
+        </div>
+    );
+}
+
+export default About;
